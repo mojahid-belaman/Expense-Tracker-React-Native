bc3c12e4d56a40192ea499b067e80f5841b30a8d
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-15 18:03:25 +0100

    Adding Navigation and Configuring Navigation and Global Colors and Editing Navigation Configuration

--- App.tsx
+++ App.tsx
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
-export default function App() {
+import ManageExpense from "./screens/ManageExpense";
+import { screenOptions } from "./constants/styles";
+import RecentExpenses from "./screens/RecentExpenses";
+import AllExpenses from "./screens/AllExpenses";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const screens = [
+  {
+    name: "RecentExpenses",
+    component: RecentExpenses,
+    options: {
+      title: "Recent Expenses",
+      tabBarLabel: "Recent",
+      tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+        <Ionicons name="hourglass" color={color} size={size} />
+      ),
+    },
+  },
+  {
+    name: "AllExpenses",
+    component: AllExpenses,
+    options: {
+      title: "All Expenses",
+      tabBarLabel: "All Expenses",
+      tabBarIcon: ({ color, size }: { color: string; size: number }) => (
+        <Ionicons name="calendar" color={color} size={size} />
+      ),
+    },
+  },
+  // Add more screens as needed
+];
+
+function ExpensesOverview() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={{
+        ...screenOptions,
+      }}
+    >
+      {screens.map((screen, index) => (
+        <Stack.Screen
+          key={index}
+          name={screen.name}
+          component={screen.component}
+          options={screen.options}
+        />
+      ))}
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="ExpenseOverview"
+            component={ExpensesOverview}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="ManageExpense" component={ManageExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
