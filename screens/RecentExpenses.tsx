2a57ab570a8a4bdaebd143fe5b2df7861b803e6e
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-18 14:53:52 +0100

    create new screen named RecentExpenses and inside it there are custom component ExpenseOutput with filter data and use context api to get data

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,7 +1,23 @@
-import { Text } from "react-native";
+import { useContext } from "react";
+
+import Expenses from "../components/Expenses/Expenses";
+import { ExpenseCtx } from "../store/context/expenseContext";
+import { getDateLast7Days } from "../utils/date";
 
 function RecentExpenses() {
-  return <Text>RecentExpenses Screen</Text>;
+  const { expenses } = useContext(ExpenseCtx);
+  const last7Days = getDateLast7Days(new Date(), 7);
+  const recentExpenses = expenses.filter(
+    (expense) => expense.date >= last7Days && expense.date <= new Date()
+  );
+
+  return (
+    <Expenses
+      expenses={recentExpenses}
+      period="Last 7 Days"
+      fallBackText="No Expenses Registered in the Last 7 Days."
+    />
+  );
 }
 
 export default RecentExpenses;
