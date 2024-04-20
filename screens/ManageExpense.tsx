f0a1e7f030eb8421f167f85d26702ac1dc7c98e3
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-20 19:09:30 +0100

    managing the loading state

--- screens/ManageExpense.tsx
+++ screens/ManageExpense.tsx
@@ -1,4 +1,4 @@
-import { useContext, useLayoutEffect } from "react";
+import { useContext, useLayoutEffect, useState } from "react";
 import { Alert, StyleSheet, View } from "react-native";
 
 import { PropsManageExpense } from "../types/screenType";
@@ -6,8 +6,11 @@ import IconButton from "../components/UI/IconButton";
 import { colors } from "../constants/styles";
 import { ExpenseCtx, expenseType } from "../store/context/expenseContext";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { editExpense, postExpense, trashExpense } from "../utils/api";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
 
 const ManageExpense = ({ navigation, route }: PropsManageExpense) => {
+  const [loading, setLoading] = useState<boolean>(false);
   const { expenses, addExpense, updateExpense, deleteExpense } =
     useContext(ExpenseCtx);
   const editedExpenseId = route.params?.expenseId;
@@ -31,7 +34,9 @@ const ManageExpense = ({ navigation, route }: PropsManageExpense) => {
         },
         {
           text: "DELETE",
-          onPress() {
+          async onPress() {
+            setLoading(true);
+            await trashExpense(editedExpenseId);
             deleteExpense(editedExpenseId);
             navigation.goBack();
           },
@@ -40,9 +45,20 @@ const ManageExpense = ({ navigation, route }: PropsManageExpense) => {
     }
   }
 
-  function confirmHandler(expense: Omit<expenseType, "id">) {
-    if (isEdited) updateExpense({ id: editedExpenseId, ...expense });
-    else addExpense(expense);
+  async function confirmHandler(expense: Omit<expenseType, "id">) {
+    if (isEdited) {
+      setLoading(true);
+      updateExpense({ id: editedExpenseId, ...expense });
+      await editExpense({ id: editedExpenseId, ...expense });
+    } else {
+      setLoading(true);
+      const id: string = await postExpense(expense);
+      const expenseData = {
+        id,
+        ...expense,
+      };
+      addExpense(expenseData);
+    }
     navigation.goBack();
   }
 
@@ -55,17 +71,25 @@ const ManageExpense = ({ navigation, route }: PropsManageExpense) => {
       <ExpenseForm
         submitLabel={isEdited ? "Update" : "Add"}
         fillField={getExpenseEdit}
+        loading={loading}
         onSubmit={confirmHandler}
         onCancel={cancelHandler}
       />
       <View style={styles.deleteContainer}>
-        {isEdited && (
-          <IconButton
-            icon="trash"
-            color={colors.error500}
-            size={36}
-            onPress={deleteExpenseHandler}
+        {loading ? (
+          <LoadingOverlay
+            styleContainer={styles.indicatorContainer}
+            indicatorConfig={{ size: "small", color: "white" }}
           />
+        ) : (
+          isEdited && (
+            <IconButton
+              icon="trash"
+              color={colors.error500}
+              size={36}
+              onPress={deleteExpenseHandler}
+            />
+          )
         )}
       </View>
     </View>
@@ -84,5 +108,8 @@ const styles = StyleSheet.create({
     borderTopWidth: 2,
     borderTopColor: colors.primary50,
     alignItems: "center",
+  },
+  indicatorContainer: {
+    padding: 24,
   },
 });
