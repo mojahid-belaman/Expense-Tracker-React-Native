5f08f90500660da9898ab01f2693577b35e35c05
mojahid-belaman <mojahidbelaman@gmail.com>
2024-04-20 19:08:42 +0100

    create function to fetch data from firbase and add indicatorLoading for the first mounting

--- screens/RecentExpenses.tsx
+++ screens/RecentExpenses.tsx
@@ -1,15 +1,36 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
+import { StyleSheet } from "react-native";
 
 import Expenses from "../components/Expenses/Expenses";
 import { ExpenseCtx } from "../store/context/expenseContext";
 import { getDateLast7Days } from "../utils/date";
+import { getExpenses } from "../utils/api";
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import { colors } from "../constants/styles";
 
 function RecentExpenses() {
-  const { expenses } = useContext(ExpenseCtx);
+  const { expenses, setExpenses } = useContext(ExpenseCtx);
   const last7Days = getDateLast7Days(new Date(), 7);
   const recentExpenses = expenses.filter(
     (expense) => expense.date >= last7Days && expense.date <= new Date()
   );
+
+  useEffect(() => {
+    async function fetchExpenses() {
+      const expenses = await getExpenses();
+      setExpenses(expenses);
+    }
+    fetchExpenses();
+  }, []);
+
+  if (expenses.length === 0) {
+    return (
+      <LoadingOverlay
+        styleContainer={styles.indicatorContainer}
+        indicatorConfig={{ size: "large", color: "white" }}
+      />
+    );
+  }
 
   return (
     <Expenses
@@ -21,3 +42,10 @@ function RecentExpenses() {
 }
 
 export default RecentExpenses;
+
+const styles = StyleSheet.create({
+  indicatorContainer: {
+    flex: 1,
+    backgroundColor: colors.primary700,
+  },
+});
